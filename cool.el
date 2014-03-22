c892fc1ca9d63e7b077dce4678876ae21841052f
mark.x.nelson <mark.x.nelson@oracle.com>
2014-03-22 12:55:45 +1100

    started

---
+++ cool.el
@@ -0,0 +1,51 @@
+; cool.el --- COOL mode
+
+;; Copyright (C) 2014  Mark Nelson
+
+;; Author: Mark Nelson
+;; Keywords: extensions
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+
+; provide a hook so users can run their own code when this mode is run
+
+(defvar cool-mode-hook nil)
+
+; create keymap
+
+(defvar cool-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "\C-j" 'newline-and-indent)
+    map)
+  "Keymap for COOL mode")
+
+; autoload
+
+(add-to-list 'auto-mode-alist '("\\.cl\\'" . cool-mode))
+
+; syntax highlighting
+
+(defconst cool-font-lock-keywords-1
+  (list
+   '("\\<\\(c\\(?:ase\\|lass\\)\\|e\\(?:lse\\|sac\\)\\|f\\(?:alse\\|i\\)\\|i\\(?:nherits\\|svoid\\|[fn]\\)\\|l\\(?:et\\|oop\\)\\|n\\(?:ew\\|ot\\)\\|of\\|pool\\|self\\|t\\(?:hen\\|rue\\)\\|while\\)\\>" . font-lock-builtin-face)
+   )
+  "Syntax highlighting for COOL keywords")
+
+(defvar cool-font-lock-keywords cool-font-lock-keywrods-1
+  "Syntax highlighting for COOL mode")
+
+
